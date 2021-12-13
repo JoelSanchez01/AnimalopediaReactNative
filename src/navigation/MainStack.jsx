626cfbf025061e5d62d5440036e36c71b33f102a
JoelSanchez01 <joeleduardosanchezherrera@gmail.com>
2021-12-13 02:25:49 -0600

    se hizo la primera pantalla de busqueda dentro de la aplicacion

--- src/navigation/MainStack.jsx
+++ src/navigation/MainStack.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 
+import MainDentro from "../pages/MainDentro";
 import Second from "../pages/Second";
 import Main from "../pages/Main";
 
@@ -11,6 +12,7 @@ const MainStack = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="MainDentro" component={MainDentro} />
         <Stack.Screen name="Main" component={Main} />
         <Stack.Screen name="Second" component={Second} />
       </Stack.Navigator>
